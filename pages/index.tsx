623c75b0d2a4056cb372b5c888417280333519ae
huy21it <huy678d@gmail.com>
2023-04-05 18:08:20 +0700

    Billboard, movielist comp

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,8 @@
+import Billboard from '@/components/Billboard';
+import MovieList from '@/components/MovieList';
 import Navbar from '@/components/Navbar';
 import useCurrentUser from '@/hooks/useCurrentUser';
+import useMovieList from '@/hooks/useMovieList';
 import { NextPageContext } from 'next';
 import { getSession } from 'next-auth/react';
 
@@ -21,10 +24,14 @@ export async function getServerSideProps(context: NextPageContext) {
 }
 
 export default function Home() {
-  
+  const { data: movies = [] } = useMovieList();
   return (
     <>
-      <Navbar/>
+      <Navbar />
+      <Billboard />
+      <div className="pb-40">
+        <MovieList title="Trending Now" data={movies} />
+      </div>
     </>
   );
 }
